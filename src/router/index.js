ac1ef59b523d189a2d7060c05baa4e34a70b800d
Le Dinh Trung <trung.le@kiaisoft.com>
2023-06-06 13:52:42 +0700

    them trang chu

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Home from "@/views/Home.vue";
 import Login from "@/views/Login.vue";
 import Register from "@/views/Resigter/Register.vue";
 import VerifyOTP from "@/views/Resigter/VerifyOTP.vue";
@@ -7,6 +8,11 @@ import RegisterInformation from "@/views/Resigter/RegisterInformation.vue";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
     {
       path: '/login',
       name: 'login',
---
+++ src/views/Home.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <main class="container">
+      <nav class="d-flex justify-space-between">
+        <img src="/favicon.ico" alt="logo">
+        <ul class="d-flex">
+          <li>
+            <router-link to="/login">Login</router-link>
+          </li>
+          <li>
+            <router-link to="/register">Register</router-link>
+          </li>
+        </ul>
+      </nav>
+    </main>
+  </div>
+</template>
+<style>
+  main {
+    height: 100vh;
+  }
+  ul {
+    list-style: none;
+  }
+  a {
+    color: #EC1C24;
+    text-decoration: none;
+  }
+  nav {
+    background-color: #ccc;
+    padding: 0.5rem;
+    font-weight: bold;
+  }
+  nav ul li {
+    margin-left: 1rem;
+  }
+</style>
+<script>
+  export default {
+    name: 'Home'
+  }
+</script>
