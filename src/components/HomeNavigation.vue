2ef2544b75289624924c26bba7dc4fa8b7f13f34
bebaocute1010 <xduc24122002@gmail.com>
2023-06-17 10:22:53 +0700

    thong bao

--- src/components/HomeNavigation.vue
+++ src/components/HomeNavigation.vue
@@ -22,12 +22,25 @@
           </div>
         </v-list-item>
 
-        <v-list-item value="notify" key="notify" class="user-not-select item-badge">
-          <v-badge v-if="number_notify > 0" :content="number_notify" color="#0074BD">
-            <v-icon>mdi-bell</v-icon>
-          </v-badge>
-          <v-icon v-else>mdi-bell</v-icon>
-        </v-list-item>
+        <v-menu open-on-click location="right">
+          <template v-slot:activator="{ props }">
+            <v-list-item key="notify" class="user-not-select item-badge" v-bind="props">
+              <v-badge v-if="number_notify > 0" :content="number_notify" color="#0074BD">
+                <v-icon>mdi-bell</v-icon>
+              </v-badge>
+              <v-icon v-else>mdi-bell</v-icon>
+            </v-list-item>
+          </template>
+
+          <v-list class="notifies-list">
+            <div class="notify-item">
+              <v-list-item v-for="notify in notifies" :key="notify.id">
+                <v-list-item-title>{{ notify.title }}</v-list-item-title>
+                <v-list-item-subtitle>{{ notify.content }}</v-list-item-subtitle>
+              </v-list-item>
+            </div>
+          </v-list>
+        </v-menu>
 
         <v-list-item
           value="cart"
@@ -41,7 +54,7 @@
           <v-icon v-else>mdi-cart</v-icon>
         </v-list-item>
 
-        <v-list-item value="logout" key="logout" class="user-not-select">
+        <v-list-item key="logout" class="user-not-select">
           <v-icon>mdi-logout</v-icon>
         </v-list-item>
       </v-list-item-group>
@@ -54,6 +67,28 @@ export default {
   name: "HomeNavigation",
   data() {
     return {
+      notifies: [
+        {
+          id: 1,
+          title: "Xác nhận đơn hàng",
+          content: "Đơn hàng 12345 của bạn đã được xác nhận",
+        },
+        {
+          id: 2,
+          title: "Xác nhận đơn hàng",
+          content: "Đơn hàng 12345 của bạn đã được xác nhận",
+        },
+        {
+          id: 3,
+          title: "Xác nhận đơn hàng",
+          content: "Đơn hàng 12345 của bạn đã được xác nhận",
+        },
+        {
+          id: 4,
+          title: "Xác nhận đơn hàng",
+          content: "Đơn hàng 12345 của bạn đã được xác nhận",
+        },
+      ],
       drawer: true,
       number_notify: 3,
       number_chat: 5,
@@ -65,6 +100,13 @@ export default {
 </script>
 
 <style>
+.notify-item .v-list-item{
+  padding: 12px !important;
+  border-bottom: 1px solid #eee;
+}
+.notifies-list {
+  padding: 0;
+}
 .v-navigation-drawer {
   position: sticky !important;
   position: -webkit-sticky !important;
